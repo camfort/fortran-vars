4494e991c6c449d40276579c897fe75426610ee7
Ben Orchard <thefirstmuffinman@gmail.com>
2022-08-25 13:42:01 +0100

    Eval: call out to fortran-src instead

--- src/Language/Fortran/Vars/Eval.hs
+++ src/Language/Fortran/Vars/Eval.hs
@@ -1,12 +1,13 @@
 module Language.Fortran.Vars.Eval
   ( eval
+  , eval'Orig
+  , eval'ViaFS
   , eval'
   , evalWithShortcircuit
   )
 where
 
 import           Prelude                 hiding ( fail )
-import qualified Data.Map                      as M
 import           Language.Fortran.AST           ( BinaryOp(..)
                                                 , Expression(..)
                                                 , Value(..)
@@ -30,22 +31,37 @@ import           Language.Fortran.Vars.Types    ( SymbolTableEntry(..)
                                                 , SymbolTable
                                                 )
 
+import Language.Fortran.Vars.FSRepr.Translate
+import qualified Language.Fortran.Repr as FS
+import qualified Language.Fortran.Repr.Eval.Common as FSEval
+import qualified Language.Fortran.Repr.Eval.Value as FSEval
+
+import Control.Monad.Reader
+import Control.Monad.Except
+
+import qualified Data.Map as Map
+
+eval' :: SymbolTable -> Expression a -> Either String ExpVal
+eval' = eval'ViaFS
+
 -- | Given a 'SymbolTable' and some 'Expression', evaluate that expression
 -- into a basic type and return it as an 'ExpVal' or a 'String' describing
 -- the issue that prevented the evaluation
-eval' :: SymbolTable -> Expression a -> Either String ExpVal
-eval' symTable expr = case expr of
-  ExpValue _ _ (ValVariable name) -> case M.lookup name symTable of
+eval'Orig :: SymbolTable -> Expression a -> Either String ExpVal
+eval'Orig symTable expr = case expr of
+  ExpValue _ _ (ValVariable name) -> case Map.lookup name symTable of
     Just (SParameter _ expVal) -> Right expVal
     Just _ -> Left $ "Cannot be evaluated: " ++ name ++ " is not a parameter."
     Nothing -> Left $ "Cannot find parameter : " ++ name
+
   ExpValue _ s val  -> valueToExpVal' s val
-  ExpUnary _ _ op e -> transformEither (unaryOp' op) $ eval' symTable e
+
+  ExpUnary _ _ op e -> transformEither (unaryOp' op) $ eval'Orig symTable e
   ExpBinary _ _ op e1 e2 ->
-    binaryTransformEither (binaryOp' op) (eval' symTable e1) (eval' symTable e2)
+    binaryTransformEither (binaryOp' op) (eval'Orig symTable e1) (eval'Orig symTable e2)
   ExpFunctionCall _ _ (ExpValue _ _ function) (AList _ _ args) ->
     transformEitherList intrinsicFunctionCall'
-      $   eval' symTable
+      $   eval'Orig symTable
       .   argExtractExpr
       <$> args
    where
@@ -54,6 +70,32 @@ eval' symTable expr = case expr of
     functionName (ValIntrinsic name) = name
     functionName _                   = ""
   _ -> Left $ "Unsupported expression at: " ++ show (getSpan expr)
+
+-- | Given a 'SymbolTable' and some 'Expression', attempt to evaluate that
+--   expression into a value in fortran-src's representation, translate it into
+--   an 'ExpVal', and return.
+eval'ViaFS :: SymbolTable -> Expression a -> Either String ExpVal
+eval'ViaFS symt expr = flip runReader symt $ runExceptT $ do
+    x <- withExceptT show $ FSEval.evalExpr expr
+    ExceptT $ return $ translateFValue x
+
+--newtype Eval = Eval { unEval :: ExceptT Error (Reader SymbolTable) }
+
+-- | Evaluate Fortran values in SymbolTable using the fortran-src
+--   representation, implicitly translating between the representations.
+instance FSEval.MonadEval (ExceptT FSEval.Error (Reader SymbolTable)) where
+    type EvalTo (ExceptT FSEval.Error (Reader SymbolTable)) = FS.FValue
+    lookupFVar name = do
+        symt <- ask
+        case Map.lookup name symt of
+          Nothing -> return Nothing
+          Just (SParameter _ val) ->
+            return $ Just $ FS.MkFScalarValue $ translateExpVal val
+          _ -> error "TODO"
+
+    -- | Ignore warnings. fortran-vars doesn't have a method to report warnings
+    --   during evaluation.
+    warn _ = pure ()
 
 -- | Given a 'SymbolTable' and some 'Expression', evaluate that expression
 -- into a basic type and return it as an 'ExpVal'
@@ -93,4 +135,4 @@ evalWithShortcircuit symTable expr = case expr of
           (Or , Right True , _          ) -> Right $ Logical True
           (Or , _          , Right True ) -> Right $ Logical True
           _ -> binaryTransformEither (binaryOp' op) e1' e2'
-  _ -> eval' symTable expr
+  _ -> eval'Orig symTable expr
--- src/Language/Fortran/Vars/Repr.hs
+++ src/Language/Fortran/Vars/Repr.hs
@@ -1,19 +1,33 @@
 module Language.Fortran.Vars.Repr
-  ( SemType
-  , Type
-  , module Language.Fortran.Vars.ReprFS.Type
-  , FVOld.CharacterLen(..)
-  , ExpVal
-  , module Language.Fortran.Vars.ReprFS.Value
-  , FVOld.Kind
-  , FVOld.Dimensions
+  ( Type
+  , F.SemType(..)
+  , F.CharacterLen(..)
+  , ExpVal(..)
+  , F.Kind
+  , F.Dimensions
   ) where
 
-import qualified Language.Fortran.Repr as FS
-import Language.Fortran.Vars.ReprFS.Type
-import Language.Fortran.Vars.ReprFS.Value
-import qualified Language.Fortran.Analysis.SemanticTypes as FVOld
+import qualified Language.Fortran.Analysis.SemanticTypes as F
+import qualified Language.Fortran.AST.Literal.Boz as F
+import Language.Fortran.Extras.JSON.Literals()
 
-type SemType = FS.FType
-type Type    = SemType
-type ExpVal = FS.FScalarValue
+import Language.Fortran.Vars.Orphans()
+
+import Data.Aeson ( ToJSON, FromJSON )
+import Control.DeepSeq ( NFData )
+import GHC.Generics ( Generic )
+import Data.Data ( Data )
+
+-- | WARNING: This type synonym may cause hassle with later GHC versions
+--   (>=8.x?), due to the kind of concrete types changing from @*@ to @Type@.
+type Type = F.SemType
+
+-- | The evaluated value of a FORTRAN expression.
+data ExpVal
+  = Int     Int
+  | Real    Double
+  | Str     String
+  | Logical Bool
+  | Boz     F.Boz
+    deriving stock (Eq, Ord, Show, Data, Generic)
+    deriving anyclass (NFData, ToJSON, FromJSON)
